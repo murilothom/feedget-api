2523ee09bc08a6f6e1871347638eb1c50c413286
Murilo Thom <murilothom.2002@hotmail.com>
2022-08-12 18:13:13 -0300

    aplicando principios solid

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from "nodemailer";
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "5bd365d977f779",
+    pass: "11217f615738c5",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Murilo Thom <contato@murilothom.com>",
+      subject,
+      html: body,
+    });
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,26 +1,19 @@
 import express from "express";
-import nodemailer from "nodemailer";
-import { prisma } from "./prisma";
+import { NodemailerMailAdapter } from "./adapters/nodemailer/nodemailer-mail-adapter";
 import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository";
 import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 
 export const routes = express.Router();
 
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
-  auth: {
-    user: "5bd365d977f779",
-    pass: "11217f615738c5",
-  },
-});
-
 routes.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
   const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+  const nodemailerMailAdapter = new NodemailerMailAdapter();
+
   const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-    prismaFeedbacksRepository
+    prismaFeedbacksRepository,
+    nodemailerMailAdapter
   );
 
   await submitFeedbackUseCase.execute({
@@ -28,18 +21,6 @@ routes.post("/feedbacks", async (req, res) => {
     comment,
     screenshot,
   });
-
-  // await transport.sendMail({
-  //   from: "Equipe Feedget <oi@feedget.com>",
-  //   to: "Murilo Thom <contato@murilothom.com>",
-  //   subject: "Novo feedback",
-  //   html: [
-  //     `<div style="font-family: sans-serif; font-size: 16px; color: #222">`,
-  //     `<p>Tipo do feedback: ${type}</p>`,
-  //     `<p>Comentário: ${comment}</p>`,
-  //     `</div>`,
-  //   ].join("\n"),
-  // });
 
   return res.status(201).send();
 });
