d89d0435496dbeebb4f3cc7254bfdcdee1847eab
Murilo Thom <murilothom.2002@hotmail.com>
2022-08-12 17:58:38 -0300

    aplicando principios solid

---
+++ src/routes.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import nodemailer from "nodemailer";
+import { prisma } from "./prisma";
+import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository";
+import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
+
+export const routes = express.Router();
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "5bd365d977f779",
+    pass: "11217f615738c5",
+  },
+});
+
+routes.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+  const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+    prismaFeedbacksRepository
+  );
+
+  await submitFeedbackUseCase.execute({
+    type,
+    comment,
+    screenshot,
+  });
+
+  // await transport.sendMail({
+  //   from: "Equipe Feedget <oi@feedget.com>",
+  //   to: "Murilo Thom <contato@murilothom.com>",
+  //   subject: "Novo feedback",
+  //   html: [
+  //     `<div style="font-family: sans-serif; font-size: 16px; color: #222">`,
+  //     `<p>Tipo do feedback: ${type}</p>`,
+  //     `<p>Comentário: ${comment}</p>`,
+  //     `</div>`,
+  //   ].join("\n"),
+  // });
+
+  return res.status(201).send();
+});
