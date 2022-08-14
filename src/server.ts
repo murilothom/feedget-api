90eeb2e15fa9e9b8309cf4683035abc6852d7c7a
Murilo Thom <murilothom.2002@hotmail.com>
2022-08-13 22:51:59 -0300

    ajustes para deploy

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
 import { routes } from "./routes";
 
 const app = express();
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("HTTP server is running!");
 });
